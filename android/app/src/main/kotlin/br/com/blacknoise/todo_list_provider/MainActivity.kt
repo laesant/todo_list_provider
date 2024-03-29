efe80b82d5fff28d4b00503c5278e157e0fab623
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-03-29 16:36:41 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/blacknoise/todo_list_provider/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.blacknoise.todo_list_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/app_widget.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
